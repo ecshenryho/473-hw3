cea0afa758da674e7ac8328ccdebadee6e12c6b7
ecshenryho <ecshenryho@gmail.com>
2018-07-12 12:14:55 -0700

    add submit handler and extracting data

--- cofferun/scripts/formhandler.js
+++ cofferun/scripts/formhandler.js
@@ -12,6 +12,20 @@
       throw new Error('Could not find element with selector: '+ selector);
     }
   }
+
+  Formhandler.prototype.addSubmitHandler = function(){
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit',function (event){
+      event.preventDefault();
+
+      var data = {};
+      $(this).serializeArray().forEach(function (item){
+        data[item.name]=item.value;
+        console.log(item.name + ' is ' + item.value);
+      });
+      console.log(data);
+    });
+  };
   App.Formhandler = Formhandler;
   window.App = App;
 })(window);
