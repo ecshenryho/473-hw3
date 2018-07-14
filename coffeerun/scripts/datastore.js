c3873ae6b9740435a75dc8be1ec9fc1390d812f0
ecshenryho <ecshenryho@gmail.com>
2018-07-13 21:49:04 -0700

    add spaces and files

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -1,20 +1,20 @@
 (function (window) {
-  'use strict';
+  "use strict";
   var App=window.App || {};
 
-  function DataStore() {this.data={};}
-  DataStore.prototype.add=function(key,val){
+  function Datastore() {this.data={};}
+  Datastore.prototype.add=function(key,val){
     this.data[key]=val;
   };
-  DataStore.prototype.get=function (key) {
+  Datastore.prototype.get=function (key) {
     return this.data[key];
   };
-  DataStore.prototype.getAll=function(){
+  Datastore.prototype.getAll=function(){
     return this.data;
   };
-  DataStore.prototype.remove=function (key) {
+  Datastore.prototype.remove=function (key) {
     delete this.data[key];
   };
-  App.DataStore=DataStore;
+  App.Datastore=Datastore;
   window.App=App;
 })(window);
--- coffeerun/scripts/truck.js
+++ coffeerun/scripts/truck.js
@@ -1,27 +1,27 @@
 (function (window) {
-  'use strict';
-  var App =window.App || {};
+  "use strict";
+  var App = window.App || {};
   function Truck(truckId, db) {
-    this.truckId=truckId;
-    this.db=db;
+    this.truckId = truckId;
+    this.db = db;
   }
-  Truck.prototype.creatOrder=function (order){
-    console.log('Adding order for '+ order.emailAddress);
+  Truck.prototype.creatOrder = function (order){
+    console.log("Adding order for "+ order.emailAddress);
     this.db.add(order.emailAddress, order);
   };
-  Truck.prototype.deliverOrder=function (customerId){
-    console.log('Delivering order for '+ customerId);
+  Truck.prototype.deliverOrder = function (customerId){
+    console.log("Delivering order for " + customerId);
     this.db.remove(customerId);
   };
-  Truck.prototype.printOrders=function(){
-    var customerIdArray=Object.keys(this.db.getAll());
+  Truck.prototype.printOrder = function(){
+    var customerIdArray = Object.keys(this.db.getAll());
 
-    console.log('Truck #' +this.truckId + ' has pending orders: ');
+    console.log("Truck #" +this.truckId + " has pending orders: ");
     customerIdArray.forEach(function (id){
       console.log(this.db.get(id));
     }.bind(this));
   };
-  App.Truck=Truck;
-  window.App=App;
+  App.Truck = Truck;
+  window.App = App;
 
 })(window);
